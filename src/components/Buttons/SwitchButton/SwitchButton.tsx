74509a040b36a26c5e1f7f53f6c6f9d373725c37
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-25 18:01:14 +0300

    savee

--- src/components/Buttons/SwitchButton/SwitchButton.tsx
+++ src/components/Buttons/SwitchButton/SwitchButton.tsx
@@ -3,20 +3,12 @@ import React, { FC } from "react";
 
 import { SwitchButtonProps } from "./SwitchButton.type";
 import styles from "./SwitchButton.module.scss";
-import { Icon } from "@/components";
-import { IconsEnum } from "@/types";
 
 const SwitchButton: FC<SwitchButtonProps> = ({ classNames }) => {
   return (
     <label className={`${styles["switch"]} ${classNames}`}>
-      <span>
-        <Icon size={20} icon={IconsEnum.Light} />
-      </span>
-      <input type="checkbox" checked name="theme" />
-      <i></i>
-      <span>
-        <Icon size={20} icon={IconsEnum.Dark} />
-      </span>
+      <input className={styles["field"]} type={styles["checkbox"]} />
+      <span className={styles["slider"]}></span>
     </label>
   );
 };
