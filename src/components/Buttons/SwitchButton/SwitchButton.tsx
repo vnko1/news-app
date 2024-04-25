51aa907a7e413b37b8fb4ce4d8587952fa212ce5
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-25 18:07:35 +0300

    save

--- src/components/Buttons/SwitchButton/SwitchButton.tsx
+++ src/components/Buttons/SwitchButton/SwitchButton.tsx
@@ -1,13 +1,14 @@
 "use client";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 import { SwitchButtonProps } from "./SwitchButton.type";
 import styles from "./SwitchButton.module.scss";
 
 const SwitchButton: FC<SwitchButtonProps> = ({ classNames }) => {
+  const [checked, setChecked] = useState(false);
   return (
     <label className={`${styles["switch"]} ${classNames}`}>
-      <input className={styles["field"]} type={styles["checkbox"]} />
+      <input checked className={styles["field"]} type={styles["checkbox"]} />
       <span className={styles["slider"]}></span>
     </label>
   );
