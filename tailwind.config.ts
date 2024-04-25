d409474c287dd0af3fcf3216db63c565b5ff8ea2
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-25 10:24:31 +0300

    feature: add base presets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,74 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "selector",
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        mob: "320px",
+        tab: "768px",
+        desc: "12800px",
+      },
+
+      spacing: {
+        "0-sm": "2px",
+        "0-md": "4px",
+        "0-lg": "6px",
+        "0-xl": "8px",
+        "1-xs": "10px",
+        "1-sm": "12px",
+        "1-md": "14px",
+        "1-lg": "16px",
+        "1-xl": "18px",
+        "2-xs": "20px",
+        "2-sm": "22px",
+        "2-md": "24px",
+        "2-lg": "26px",
+        "2-xl": "28px",
+        "3-xs": "30px",
+        "3-sm": "32px",
+        "3-md": "34px",
+        "3-lg": "36px",
+        "3-xl": "38px",
+        "4-xs": "40px",
+        "4-sm": "42px",
+        "4-md": "44px",
+        "4-lg": "46px",
+        "4-xl": "48px",
+        "5-xs": "50px",
+        "5-sm": "52px",
+        "5-md": "54px",
+        "5-lg": "56px",
+        "5-xl": "58px",
+      },
+      colors: {
+        accent: "#4440F6",
+        black: "#111321",
+        grey: "#A8A8A8",
+        white: "#FFFFFF",
+      },
+      fontSize: {
+        xxs: "10px",
+        xs: "12px",
+        sm: "14px",
+        base: "16px",
+        md: "18px",
+        lg: "24px",
+        xl: "32px",
+        xxl: "42px",
+      },
+      fontFamily: {
+        manrope: ["var(--font-manrope)"],
+        roboto: ["var(--font-roboto)"],
+        poppins: ["var(--font-poppins)"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
