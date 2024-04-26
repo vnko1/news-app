ff66c9a53afaf92c113ace2aba1f60c5474fb94f
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-26 15:37:41 +0300

    save

--- src/app/UI/Header/components/ThemeToggler/ThemeToggler.type.ts
+++
@@ -1,7 +0,0 @@
-import { Dispatch, SetStateAction } from "react";
-
-export type ThemeTogglerProps = {
-  classNames?: string;
-  checked: boolean;
-  setChecked: Dispatch<SetStateAction<boolean>>;
-};
