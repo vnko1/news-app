baf4190fc5c16a32211e1553ef2cbff584accdc0
Andrii Valenko <andreyvalenko@gmail.com>
2024-04-30 12:59:39 +0300

    feat: add weather api

--- src/app/(news)/(home)/ui/Weather/Weather.tsx
+++ src/app/(news)/(home)/ui/Weather/Weather.tsx
@@ -1,9 +1,35 @@
-import React, { FC } from "react";
+"use client";
+import React, { FC, useEffect, useState } from "react";
+
+import { CurrentWeatherApiResponseType } from "@/types";
+import { getCurrentWeather } from "@/lib";
 
 import { WeatherProps } from "./Weather.type";
 import styles from "./Weather.module.scss";
 
 const Weather: FC<WeatherProps> = () => {
+  const [currentWeather, setCurrentWeather] =
+    useState<null | CurrentWeatherApiResponseType>(null);
+
+  currentWeather;
+
+  useEffect(() => {
+    async function getPosition(position: GeolocationPosition) {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+
+      const res = await getCurrentWeather(lat, lon);
+      setCurrentWeather(res);
+    }
+    async function onErrorPosition() {
+      const res = await getCurrentWeather();
+
+      setCurrentWeather(res);
+    }
+
+    navigator.geolocation.getCurrentPosition(getPosition, onErrorPosition);
+  }, []);
+
   return <div className={styles["weather"]}>Weather</div>;
 };
 
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,2 @@
 export * from "./actions/news";
+export * from "./actions/weather";
