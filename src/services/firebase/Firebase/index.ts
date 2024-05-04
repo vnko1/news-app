82e17beabcd6fe4025cc382b59873a8bea7eae6f
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-05-04 08:40:40 +0300

    save

--- src/services/firebase/Firebase/index.ts
+++ src/services/firebase/Firebase/index.ts
@@ -1,13 +1,20 @@
 import { FirebaseApp, initializeApp, getApps, getApp } from "firebase/app";
 
+const apiKey = process.env.F_API_KEY;
+const authDomain = process.env.AUTH_DOMAIN;
+const projectId = process.env.PROJECT_ID;
+const storageBucket = process.env.STORAGE_BUCKET;
+const messagingSenderId = process.env.MESSAGING_SENDER_ID;
+const appId = process.env.APP_ID;
+
 abstract class FireBaseApi {
   private firebaseConfig = {
-    apiKey: process.env.F_API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
+    apiKey,
+    authDomain,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
   };
   protected app: FirebaseApp;
 
