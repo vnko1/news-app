35d723cd169fb4b7eb997478c91f65117f21cf7a
Andrii Valenko <andreyvalenko@gmail.com>
2024-05-07 14:21:40 +0300

    save

--- src/app/(news)/read/ui/Reads/Reads.tsx
+++ src/app/(news)/read/ui/Reads/Reads.tsx
@@ -1,12 +1,21 @@
 "use client";
-import { useProfileContext } from "@/context";
-import formatDate from "@/utils/formatDate";
 import React, { FC } from "react";
+import { useProfileContext } from "@/context";
+import { DBResponseType } from "@/types";
 
+function getReadsData(readData: DBResponseType | null) {
+  if (!readData) return null;
+  const readArr = Object.keys(readData);
+  const d = readArr.reduce((acc, el) => {
+    console.log(readData[el]);
+  }, {});
+
+  return null;
+}
 const Reads: FC = () => {
   const { read } = useProfileContext();
 
-  console.log(formatDate());
+  getReadsData(read);
 
   return <div>Reads</div>;
 };
